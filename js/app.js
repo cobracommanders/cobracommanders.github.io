cd0d571c09c2869771cd206ecc5def20f6239bca
Jason Barnes <jason@sparkview.io>
2017-09-01 18:35:28 -0700

    First Commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// Dom7
+var $ = Dom7;
+
+// Theme
+var theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
+// Init App
+var app = new Framework7({
+  root: '#app',
+  theme: theme,
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+    };
+  },
+  routes: routes,
+});
